9c7cdcedc1227dd8a6559b4764f403bd98fcbb7d
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-22 14:31:32 +0000

    Changed variable names

--- printf.c
+++ printf.c
@@ -1,36 +1,38 @@
 #include "main.h"
+
 /**
  * _printf - is a mini printf
- * @theFormat: is a pointer to the string to be printed and handled
+ * @fmt: is a pointer to the string to be printed and handled
  * Return: is to return the number of the printed characters
  */
-int _printf(const char *theFormat, ...)
-{
-	int fp = 0, thePchar = 0, theArgNum, tmp, bp = 0;
-	va_list myPtr;
-	char myBuffer[1024];
 
-	if (theFormat == NULL)
+int _printf(const char *fmt, ...)
+{
+	int fpos = 0, wrtnchar = 0, argnum, tmp, bpos = 0;
+	va_list ap;
+	char buf[1024];
+
+	if (fmt == NULL)
 		return (-1);
-	theArgNum = argCounting(theFormat);
-	va_start(myPtr, theFormat);
-	while (theFormat[fp] != '\0')
+	argnum = argCounting(fmt);
+	va_start(ap, fmt);
+	while (fmt[fpos] != '\0')
 	{
-		NormalText(theFormat, &fp, &thePchar, myBuffer, &bp);
-		tmp = thePchar;
-		if (theFormat[fp] == '%')
+		NormalText(fmt, &fpos, &wrtnchar, buf, &bpos);
+		tmp = wrtnchar;
+		if (fmt[fpos] == '%')
 		{
-			thePchar += opDeterminer(theFormat, &theArgNum, &fp, myPtr, myBuffer, &bp);
-			if (thePchar < tmp)
+			wrtnchar += opDeterminer(fmt, &argnum, &fpos, ap, buf, &bpos);
+			if (wrtnchar < tmp)
 			{
-				va_end(myPtr);
+				va_end(ap);
 				return (-1);
 			}
 		}
-		if (bp > 1023)
-			printing(myBuffer, &bp);
+		if (bpos > 1023)
+			printing(buf, &bpos);
 	}
-	printing(myBuffer, &bp);
-	va_end(myPtr);
-	return (thePchar);
+	printing(buf, &bpos);
+	va_end(ap);
+	return (wrtnchar);
 }
